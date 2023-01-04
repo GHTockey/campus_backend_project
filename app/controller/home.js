c01cca1202402cab7226b85f6b4a3010e3eb8b2b
Tockey <tockey@yeah.net>
2023-01-04 18:13:11 +0800

    测试2

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const { Controller } = require('egg');
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'eggjs 服务已启动';
+    ctx.body = 'eggjs 服务已启动1';
   }
 }
 
