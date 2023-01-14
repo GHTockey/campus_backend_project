10c5ea441c97a6a21826ef8260cefd089e7da5ef
Tockey <tockey@yeah.net>
2023-01-14 22:57:19 +0800

    修改：重写了提交文章时的字段校验业务逻辑，实现热提示缺省参数

--- app/middleware/checkAddArticleParams.js
+++ app/middleware/checkAddArticleParams.js
@@ -1,30 +1,19 @@
 // 校验主页文章参数
 module.exports = function checkAddArticleParams() {
     return async function (ctx, next) {
-        let {
-            userID,
-            name,
-            avatar,
-            title,
-            describe,
-            content,
-            cover,
-            date,
-            is_concern,
-            is_like,
-            is_topping,
-            is_boutique,
-            is_collection,
-            views
-        } = ctx.request.body;
-        console.log(ctx.request.body);
-        if (!!userID && !!name && !!avatar && !!title && !!describe && !!content && !!cover && !!date && !!String(is_concern) && !!String(is_like) && !!String(is_topping) && !!String(is_boutique) && !!String(is_collection) && !!String(views)) {
-            await next(); // 校验通过
-        } else {
-            ctx.body = {
-                code: 400,
-                message: "参数缺失, 请检查是否传递参数: userID/name/avatar/title/describe/content/cover/date/is_concern/is_like/is_topping/is_boutique/is_collection/views"
-            }
-        }
+        // 必要的字段
+        let checkFields = ['userID', 'name', 'avatar', 'title', 'describe', 'content', 'cover', 'date', 'is_concern', 'is_like', 'is_topping', 'is_boutique', 'is_collection', 'views', 'cate'];
+        // 缺少的字段
+        let lackFields = [];
+        // 遍历必要的字段名
+        checkFields.forEach(item => {
+            // 判断空值
+            if (!(ctx.request.body[item] || ctx.request.body[item] == 0)) return lackFields.push(item);
+        });
+        if (!ctx.request.body.cate) lackFields.push('cate');
+        if (!lackFields.length) return await next(); // 校验通过
+
+        ctx.body = { code: 400, message: `参数缺失: ${lackFields}` }
+
     }
 };
