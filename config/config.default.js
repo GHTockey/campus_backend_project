d11a31e3e7d5c633399db3abccf23ca2f3db218b
Tockey <tockey@yeah.net>
2023-01-25 21:11:12 +0800

    修改：数据库配置回环地址

--- config/config.default.js
+++ config/config.default.js
@@ -39,10 +39,10 @@ module.exports = appInfo => {
   // mysql 配置
   config.mysql = {
     client: {
-      host: '1.15.48.103',
+      host: '127.0.0.1',
       port: '3306',
-      user: 'campus',
-      password: 'EYCsxBY4CZTXJGMd',
+      user: 'root',
+      password: 'eb021e1242ab27eb',
       database: 'campus',
     }
   };
