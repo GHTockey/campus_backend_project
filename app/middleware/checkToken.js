53ba4fbab60e22178279863cfcc2ab000df6d301
Tockey <tockey@yeah.net>
2023-02-01 19:26:56 +0800

    修复：错误的数据表名称

--- app/middleware/checkToken.js
+++ app/middleware/checkToken.js
@@ -1,15 +1,16 @@
 module.exports = (isAdmin) => {
     return async function checkToken(ctx, next) {
         try {
-            // 从请求头获取 token
+            // 从请求头获取 token authorization
             let token = ctx.get('Authorization').replace('Bearer ', '');
+            console.log(token);
             // 校验 token
             let checkRes = ctx.app.jwt.verify(token, ctx.app.config.jwt.secret);
             let { username } = checkRes;
             // console.log(username);
             if (isAdmin) {
                 // 管理员操作
-                let checkAdmin = await ctx.app.mysql.select('administrators', { where: { username } });
+                let checkAdmin = await ctx.app.mysql.select('users', { where: { username } });
                 if (!checkAdmin.length) return ctx.body = { code: 400, message: `此操作需要管理员权限` };
                 // 管理员校验通过
                 await next();
